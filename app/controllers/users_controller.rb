8dcabce95f1337ab7d0a5fd6a20e11f726cd9d1e
Yuto20 <yuto1178@gmail.com>
2021-09-10 11:32:59 +0900

    ユーザー一覧ページの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  def index
+    query = "SELECT * FROM users"
+    @users = User.find_by_sql(query)
+  end
+  
   def show
     @user = User.find(params[:id])
   end
