7aa280f6b714dfe3830a5597c32ed00b296ff616
Yuto20 <yuto1178@gmail.com>
2021-09-12 15:27:27 +0900

    playsテーブルをmigrate

--- db/migrate/20210912062407_create_plays.rb
+++ db/migrate/20210912062407_create_plays.rb
@@ -1,7 +1,8 @@
 class CreatePlays < ActiveRecord::Migration[6.0]
   def change
     create_table :plays do |t|
-
+      t.references :game, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_12_044930) do
+ActiveRecord::Schema.define(version: 2021_09_12_062407) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -41,6 +41,15 @@ ActiveRecord::Schema.define(version: 2021_09_12_044930) do
     t.index ["ancestry"], name: "index_games_on_ancestry"
   end
 
+  create_table "plays", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "game_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["game_id"], name: "index_plays_on_game_id"
+    t.index ["user_id"], name: "index_plays_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -59,4 +68,6 @@ ActiveRecord::Schema.define(version: 2021_09_12_044930) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "plays", "games"
+  add_foreign_key "plays", "users"
 end
