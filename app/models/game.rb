3e3d1e641d14d7f741949154b15799d9d6997d46
Yuto20 <yuto1178@gmail.com>
2021-09-13 21:34:54 +0900

    gameモデル・playモデルの削除とjQueryの導入

--- app/models/game.rb
+++
@@ -1,8 +0,0 @@
-class Game < ApplicationRecord
-  has_many :plays, dependent: :destroy
-  has_many :users, through: :plays
-  has_ancestry
-  validates_associated :users
-
-  validates :platform, presence: true
-end
