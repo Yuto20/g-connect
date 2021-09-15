59670b89a2249323cf95dc45a798e45f66bd403c
Yuto20 <yuto1178@gmail.com>
2021-09-15 10:58:59 +0900

    一覧ページのcurrent_userを非表示とusers_controllerのコードのリファクタリング

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,17 @@
 class UsersController < ApplicationController
   before_action :search_user, only: [:index, :search]
+  before_action :set_user, only: [:show, :edit, :update]
   def index
-    query = "SELECT * FROM users"
-    @users = User.find_by_sql(query)
+    @users = User.where.not(id: current_user.id).order("created_at DESC")
   end
   
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to action: :show
     else
@@ -30,6 +27,10 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:image, :nickname, :age_id, :sex_id, :voice_id, :platform_id, :favorite_id, :profile)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
   def search_user
