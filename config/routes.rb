25617220baada2f41e5797b53f53ce3d0ca80b1f
Yuto20 <yuto1178@gmail.com>
2021-09-15 15:56:57 +0900

    relationshipsのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   root to: "tops#index"
   get 'users/search'
   resources :tops, only: :index
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get :followings, on: :member
+    get :followers, on: :member
+  end
 end
