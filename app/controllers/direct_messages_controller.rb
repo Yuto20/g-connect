e6a4be0252bf76a6046790350f8da7f5478b2b1d
Yuto20 <yuto1178@gmail.com>
2021-09-18 18:56:24 +0900

    チャット機能の実装

--- app/controllers/direct_messages_controller.rb
+++ app/controllers/direct_messages_controller.rb
@@ -1,2 +1,28 @@
 class DirectMessagesController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    rooms = current_user.entries.pluck(:room_id)
+    entries = Entry.find_by(user_id: @user.id, room_id: rooms)
+
+    if entries.nil?
+      @room = Room.new
+      @room.save
+      Entry.create(user_id: @user.id, room_id: @room.id)
+      Entry.create(user_id: current_user.id, room_id: @room.id)
+    else
+      @room = entries.room
+    end
+    @direct_messages = @room.direct_messages
+    @direct_message = DirectMessage.new(room_id: @room.id)
+  end
+
+  def create
+    @direct_message = current_user.direct_messages.new(direct_message_params)
+    @direct_message.save
+  end
+
+  private
+  def direct_message_params
+    params.require(:direct_message).permit(:content, :room_id)
+  end
 end
